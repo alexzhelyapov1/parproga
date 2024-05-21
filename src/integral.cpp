a389973847f47ef46262d1e545d34fb9b68e9d4f
msi <->
2024-05-21 18:43:33 +0300

    1

--- src/integral.cpp
+++ src/integral.cpp
@@ -53,35 +53,75 @@ int countPeaks(double a, double b) {
 vector<double> createIntervals(double a, double b, int N) {
     vector<double> intervals;
     intervals.push_back(a);
+    std::cout << "Helio\n";
 
     // Определяем количество пиков на всем интервале
     int totalPeaks = countPeaks(a, b);
-    std::cout << "Number of pics: " << totalPeaks << std::endl;
 
-    // Вычисляем желаемое количество пиков на каждом интервале
-    int peaksPerInterval = totalPeaks / N;
 
-    if (peaksPerInterval == 0) {
-        for (int i = 0; i < totalPeaks; i++) {
-            
+    if (totalPeaks < N) {
+        double step = (b - a) / N;
+        for (int i = 1; i < N; i++) {
+            intervals.push_back(a + i * step);
         }
     }
+    else {
+        double currentPosition = a;
+        double lastPosition = a;
+        int currentPeaks = 0;
+        int peaksPerInterval = totalPeaks / N;
 
-    // Делим интервал на N частей, учитывая количество пиков
-    double currentPosition = a;
-    int currentPeaks = 0;
-    for (int i = 1; i < N; i++) {
+        for (int i = 1; i < N; i++) {
         // Находим следующую точку разбиения, пока не достигнем желаемого количества пиков
-        while (currentPeaks < i * peaksPerInterval) {
-            currentPosition += 0.01;
-            currentPeaks += countPeaks(currentPosition - 0.01, currentPosition);
-        }
+            while (currentPeaks < i * peaksPerInterval && currentPosition < b) {
+                currentPosition += 0.01;
+                currentPeaks += countPeaks(currentPosition - 0.01, currentPosition);
+                std::cout << "-\n";
+            }
+
+            if (currentPosition > b) {
+                std::cout << "Ended at " << lastPosition << std::endl;
+
+                int restIntervals = N - i;
+                for (int k = i; k < N; k++) {
+                    intervals.push_back(lastPosition + (b - a) / restIntervals * k);
+                }
+                break;
+            }
+
             intervals.push_back(currentPosition);
+            lastPosition = currentPosition;
+        }
+
     }
 
-    intervals.push_back(b);
+    // // Если пиков меньше, чем интервалов, распределяем интервалы равномерно
+    // if (totalPeaks < N) {
+    //     double step = (b - a) / N;
+    //     for (int i = 1; i < N; i++) {
+    //         intervals.push_back(a + i * step);
+    //     }
+    // } else { // Если пиков больше или равно интервалам, действуем как раньше
+    //     // Вычисляем желаемое количество пиков на каждом интервале
+    //     int peaksPerInterval = totalPeaks / N;
 
-    return intervals;
+    //     // Делим интервал на N частей, учитывая количество пиков
+    //     double currentPosition = a;
+    //     int currentPeaks = 0;
+    //     for (int i = 1; i < N; i++) {
+    //     // Находим следующую точку разбиения, пока не достигнем желаемого количества пиков
+    //         while (currentPeaks < i * peaksPerInterval && currentPosition < b) {
+    //             currentPosition += 0.01;
+    //             currentPeaks += countPeaks(currentPosition - 0.01, currentPosition);
+    //             std::cout << "-\n";
+    //         }
+    //         intervals.push_back(currentPosition);
+    //     }
+    // }
+
+  intervals.push_back(b);
+
+  return intervals;
 }
 
 
@@ -111,7 +151,7 @@ void Integral(double from, double to)
 
 double LaunchParallelIntegral(int threads_number)
 {
-    double from = 0.1, to = 10;
+    double from = 0.01, to = 10;
     double from_local, to_local;
 
     if (from > to || from <= 0) {
@@ -124,34 +164,34 @@ double LaunchParallelIntegral(int threads_number)
 
     // std::cout << "Pics number: " << first_pic << " " << last_pic << std::endl;
 
-    vector<double> intervals = createIntervals(from, to, threads_number);
+    // vector<double> intervals = createIntervals(from, to, threads_number);
 
-    for (auto i : intervals) {
-        std::cout << i << " ";
-    }
-    std::cout << "\n";
+    // for (auto i : intervals) {
+    //     std::cout << i << " ";
+    // }
+    // std::cout << "\n";
 
     
 
-    // std::vector<std::thread> threads;
+    std::vector<std::thread> threads;
 
-    // for (int i = 0; i < threads_number; i++) {
+    for (int i = 0; i < threads_number; i++) {
 
-    //     from_local = from + (to - from) / threads_number * i;
-    //     to_local = from + (to - from) / threads_number * (i + 1);
+        from_local = from + (to - from) / threads_number * i;
+        to_local = from + (to - from) / threads_number * (i + 1);
 
-    //     if (i == threads_number - 1) {
-    //         to_local = to;
-    //     }
+        if (i == threads_number - 1) {
+            to_local = to;
+        }
 
-    //     threads.push_back(std::thread(Integral, from_local, to_local));
-    // }
+        threads.push_back(std::thread(Integral, from_local, to_local));
+    }
 
-    // for (auto& thread : threads) {
-    //     thread.join();
-    // }
+    for (auto& thread : threads) {
+        thread.join();
+    }
 
-    // std::cout << "Result: " << result << std::endl;
+    std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
@@ -159,7 +199,7 @@ double LaunchParallelIntegral(int threads_number)
 
 int main(int argc, char **argv)
 {	
-    int threads_number = 2;
+    int threads_number = 8;
     LaunchParallelIntegral(threads_number);
 
     return 0;
